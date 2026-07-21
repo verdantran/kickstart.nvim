043e46bed10ec52f33717ffc63efcd92887af0c9
JB <labpd01@googlemail.com>
2026-07-21 11:07:42 +0100

    show hidden files

--- init.lua
+++ init.lua
@@ -500,7 +500,11 @@ do
     --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
     --   },
     -- },
-    -- pickers = {}
+    pickers = {
+      find_files = {
+        hidden = true
+      }
+    },
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
     },
